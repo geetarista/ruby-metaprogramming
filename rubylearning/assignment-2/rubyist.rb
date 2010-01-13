9903220968351884627c0538a8e73e5c9b0eece4
Robby Colvin <geetarista@gmail.com>
2010-01-13 13:42:25 -0700

    Major overhaul.

---
+++ rubylearning/assignment-2/rubyist.rb
@@ -0,0 +1,10 @@
+class Rubyist
+  def initialize name
+    @name = name
+    @count = 0
+  end
+
+  def say!
+    puts 'hello'
+  end
+end
---
+++ rubylearning/assignment-2/test_snippet.rb
@@ -0,0 +1,10 @@
+require 'rubyist'
+
+satish = Rubyist.new('Satish')
+3.times { satish.say! }
+
+puts '-' * 20
+
+require 'rubyist_with_count'
+
+3.times { satish.say! }
